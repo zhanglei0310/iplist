ae8b72ee9f8a211072a0c9cf7cace2d0e90d3894
metowolf <i@i-meto.com>
2021-07-11 14:06:27 +0000

    新增城市级别细化

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,6 +1,7 @@
 const { series, src, dest } = require('gulp')
 const through2 = require('through2')
 const plugin_country = require('./plugins/country')
+const plugin_city = require('./plugins/city')
 const plugin_cncity = require('./plugins/cncity')
 const plugin_china = require('./plugins/china')
 const plugin_cidrmerge = require('./plugins/cidrmerge')
@@ -11,6 +12,15 @@ const country = () => {
   return src(database)
     .pipe(through2.obj(function(file, _, cb) {
       return plugin_country(this, file, cb)
+    }))
+    .pipe(through2.obj(plugin_cidrmerge))
+    .pipe(dest('data/country'))
+}
+
+const city = () => {
+  return src(database)
+    .pipe(through2.obj(function(file, _, cb) {
+      return plugin_city(this, file, cb)
     }))
     .pipe(through2.obj(plugin_cidrmerge))
     .pipe(dest('data/country'))
@@ -34,6 +44,7 @@ const china = () => {
 }
 
 exports.country = country
+exports.city = city
 exports.cncity = cncity
 exports.china = china
-exports.build = series(country, cncity, china)
+exports.build = series(country, city, cncity, china)
---
+++ src/plugins/city.js
@@ -0,0 +1,61 @@
+const vinyl = require('vinyl')
+const IPDB = require('ipdb')
+const ipdb_range = require('@ipdb/range')
+const ProgressBar = require('progress')
+const Format = require('../format')
+
+let format = null
+
+const getCountryISO3166 = (info) => {
+  const data = format.execute([info.country, info.area].join('|'))
+  return data.iso3166_2
+}
+
+const plugin = (through2, file, cb) => {
+
+  console.log('Parse ipdb')
+
+  format = new Format({
+    cwd: 'alias/country',
+    filenameDict: ['country_name', 'region_name', 'city_name', 'district_name'],
+    dic: 'qqwry'
+  })
+
+  const ipdb = new IPDB(file.contents, {
+    patches: [ipdb_range]
+  })
+
+  let bar = new ProgressBar(':bar :current/:total', { total: ipdb.meta.node_count })
+
+  let result = []
+  let ip = '0.0.0.0'
+  while (true) {
+    const info = ipdb.find(ip).data
+    const iso3166_2 = getCountryISO3166(info)
+    if (iso3166_2.length > 3) {
+      if (!result[iso3166_2]) {
+        result[iso3166_2] = []
+      }
+      result[iso3166_2].push(`${info.range.from}/${info.bitmask}`)
+    }
+    bar.tick()
+    ip = info.range.next
+    if (ip === '0.0.0.0') break
+  }
+
+  console.log()
+
+  for (let [country_code, cidrs] of Object.entries(result)) {
+    let temp = new vinyl({
+      cwd: '/',
+      base: '/',
+      path: `/${country_code.split('-')[0]}/${country_code}.txt`,
+      contents: new Buffer.from(cidrs.join('\n'))
+    })
+    through2.push(temp)
+  }
+
+  cb()
+}
+
+module.exports = plugin
